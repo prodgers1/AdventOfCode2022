99cfff9628c00d313398b07a93afa86e9ba63ea8
Paul <pcrodgers1@gmail.com>
2022-11-30 22:24:53 -0700

    Day 1 sum calories

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -5,3 +5,18 @@ from collections import defaultdict
 sys.path.append('./')
 from adventInput import GetInput
 _input = GetInput(1)
+
+maxCalories = []
+current = 0
+
+for line in _input:
+  if len(line) == 0:
+    maxCalories.append(current)
+    current = 0
+  else:
+    current += int(line)
+maxCalories.append(current)
+
+print(sorted(maxCalories, reverse=True)[0])
+top3 = sum(sorted(maxCalories, reverse=True)[0:3])
+print(sum(top3))
