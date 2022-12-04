7b620d6660f172c1532e2d83bb408f9c11b205d2
prodgers1 <pcrodgers1@gmail.com>
2022-12-04 05:41:00 +0000

    Day 4 set operations

--- Day04/Day04.py
+++ Day04/Day04.py
@@ -5,3 +5,24 @@ from collections import defaultdict
 sys.path.append('./')
 from adventInput import GetInput
 _input = GetInput(4)
+
+part1 = 0
+part2 = 0
+
+for line in _input:
+    firstRange, secondRange = line.split(',')
+
+    first, second = firstRange.split('-')
+    third, forth = secondRange.split('-')
+
+    firstList = set([*range(int(first), int(second) + 1, 1)])
+    secondList = set([*range(int(third), int(forth) + 1, 1)])
+    
+    if firstList <= secondList or secondList <= firstList:
+        part1 += 1
+    
+    if len(set(firstList).intersection(set(secondList))) > 0:
+        part2 += 1
+
+print(part1)
+print(part2)
