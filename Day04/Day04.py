756ceb12a89be35bf1e557127a34691fc8e5ffc3
prodgers1 <pcrodgers1@gmail.com>
2022-12-04 05:50:26 +0000

    Day 4 updated with first solution

--- Day04/Day04.py
+++ Day04/Day04.py
@@ -18,8 +18,18 @@ for line in _input:
     firstList = set([*range(int(first), int(second) + 1, 1)])
     secondList = set([*range(int(third), int(forth) + 1, 1)])
     
-    if firstList <= secondList or secondList <= firstList:
+    first = int(first)
+    second = int(second)
+    third = int(third)
+    forth = int(forth)
+
+    # this was my first initial approach but had the operators swapped for the first expression -.-
+    if (first >= third and second <= forth) or (third >= first and forth <= second):
         part1 += 1
+
+    # second cleaner approach using sets to check subsets
+    #if firstList <= secondList or secondList <= firstList:
+    #    part1 += 1
     
     if len(set(firstList).intersection(set(secondList))) > 0:
         part2 += 1
