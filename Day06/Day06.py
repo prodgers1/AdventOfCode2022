59911367b4fbe231f85615d4178923187519ceef
Paul <pcrodgers1@gmail.com>
2022-12-05 22:22:38 -0700

    Day 4 find first unique subset slow AF

--- Day06/Day06.py
+++ Day06/Day06.py
@@ -4,4 +4,16 @@ from copy import deepcopy
 from collections import defaultdict
 sys.path.append('./')
 from adventInput import GetInput
-_input = GetInput(6)
+_input = GetInput(6)[0]
+
+
+def findStartOfPacketMarker(line, marker):
+  for index in range(marker, len(line), 1):
+    sub = line[index - marker:index]
+    distinct = set(sub)
+    if len(distinct) == len(sub):
+      print(index)
+      break
+
+findStartOfPacketMarker(_input, 4)
+findStartOfPacketMarker(_input, 14)
